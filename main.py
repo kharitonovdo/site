a23c93ab9163ea696e0776f28b041de8411422c0
Дмитрий <kharitonovdo@yandex.ru>
2023-04-17 15:54:05 +0300

    updatetggg

--- main.py
+++ main.py
@@ -13,34 +13,16 @@ def index():
     return render_template('index.html', news=news_list)
 
 
-@app.route('/templates/Magicka.html')
-def Magicka():
-    return render_template('Magicka.html')
-
-
-@app.route('/templates/Magicka_2.html')
-def Magicka_2():
-    return render_template('Magicka_2.html')
-
-
-@app.route('/templates/hotline_miami.html')
-def hotline_miami():
-    return render_template('hotline_miami.html')
-
-
-@app.route('/templates/hotline_miami_2.html')
-def hotline_miami_2():
-    return render_template("hotline_miami_2.html")
-
-
-@app.route('/templates/Minecraft.html')
-def Minecraft():
-    return render_template('Minecraft.html')
-
-
-@app.route('/templates/Dishonored.html')
-def Dishonored():
-    return render_template('Dishonored.html')
+@app.route('/games/<game>')
+def Magicka(game):
+    with open('games.json', 'rt', encoding='utf8') as f:
+        news_list = json.load(f)
+        print(news_list)
+        for item in news_list['games']:
+            print(item, game)
+            if game == item['html']:
+                return render_template(game)
+    return render_template("does_not_exist.html")
 
 
 if __name__ == '__main__':
